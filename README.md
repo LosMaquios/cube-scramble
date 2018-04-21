18b4186bc9d8b6e123259b1328330a6d38f4c5de
Camilo Rodríguez <aeroxmotion@hotmail.com>
2018-04-21 18:08:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Cube Scramble
+
+  Blazing fast cube scramble by using a [finite-state machine](https://en.wikipedia.org/wiki/Finite-state_machine) algorithm.
+
+## Usage
+
+```js
+const moves = [
+  'U', 'R', 'D',
+  'L', 'F', 'B'
+]
+
+scramble(moves)
+
+/**
+ * Hypothetical result:
+ *
+ * ["F'", "D'", "U2", "F2", "L2", "B2", "L", "B", "F'", "D", "L'", "B2", "D'", "L2", "B2", "D2", "F", "R'", "U'", "F2"]
+ */
+```
---
+++ index.js
@@ -0,0 +1,65 @@
+
+const defaults = {
+  variations: ['\'', '2', ''],
+  length: 20
+}
+
+/**
+ * Scramble input moves
+ *
+ * @param {Array.<string>} moves
+ * @param {Object} [options]
+ * @param {Array.<string>} [options.variations]
+ *
+ * @return {Array.<string>}
+ */
+export function scramble (moves, options) {
+  options = Object.assign({}, options, defaults)
+
+  const scrambled = []
+
+  let next = random(moves)
+  let cycle = options.length
+
+  const states = finite(moves)
+
+  while (cycle--) {
+    scrambled.push(next + random(options.variations))
+
+    next = random(states[next])
+  }
+
+  return scrambled
+}
+
+/**
+ * Create a new map of states
+ *
+ * @param {Array.<string>} moves
+ *
+ * @return {Object}
+ */
+function finite (moves) {
+  const map = {}
+
+  moves.forEach(move => {
+    const states = moves.slice()
+
+    states.splice(moves.indexOf(move), 1)
+
+    map[move] = states
+  })
+
+  return map
+}
+
+/**
+ * Get random item from a given `array`
+ *
+ * @param {Array.<*>} array
+ *
+ * @return {*}
+ */
+function random (array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
